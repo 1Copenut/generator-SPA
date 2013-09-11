e7bc779c90b7d4e7a68a3c88732b2ca13549a6d2
Trevor Pierce <tpierce@bcgtrans.com>
2013-09-11 17:06:01 -0500

    First commit for generator-SPA

---
+++ app/index.js
@@ -0,0 +1,66 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var SpaGenerator = module.exports = function SpaGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(SpaGenerator, yeoman.generators.Base);
+
+SpaGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // Have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    name: 'spaName',
+    message: 'Would you like to call this single-page app?'
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.spaName = props.spaName;
+
+    cb();
+  }.bind(this));
+};
+
+// Copy the package.json to get my local Express server, Node modules
+SpaGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+// Copy the Gruntfile.js
+SpaGenerator.prototype.gruntfile = function gruntfile() {
+	this.template('Gruntfile.js');
+}
+
+SpaGenerator.prototype.app = function app() {
+  // Create the core app directory
+	this.mkdir('app');
+  this.mkdir('app/templates');
+  this.mkdir('app/css');
+  this.mkdir('app/sass');
+  this.mkdir('app/imgs');
+  this.mkdir('app/scripts');
+
+	// Create the test directory
+	this.mkdir('test');
+
+  // this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+SpaGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		
+		jshint: {
+			files: ["public/js/*.js", "!public/js/config.js"],
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true,
+					define: true
+			}
+		},
+		
+		compass: {
+			dev: {
+				options: {
+					sassDir: "app/styles/sass",
+					cssDir: "app/styles/css"
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-jquerybuilder");
+	grunt.loadNpmTasks("grunt-bootstrap");
+	grunt.loadNpmTasks("grunt-contrib-compass");
+	
+	grunt.registerTask("default", ["compass"]);
+};
